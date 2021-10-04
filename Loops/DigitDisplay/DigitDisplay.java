af27af9d689d22763a37d55a534d81b7ac8cd02a
pschake34 <pschake34@charlottesvilleschools.org>
2021-10-04 09:54:00 -0400

    Start DigitDisplay

---
+++ Loops/DigitDisplay/DigitDisplay.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class DigitDisplay {
+   public static void main(String[] args) {
+      Scanner input = new Scanner(System.in);
+      int number;
+      
+      System.out.print("Enter a positive integer: ");
+      number = input.nextInt();
+      
+      
+   }
+}
