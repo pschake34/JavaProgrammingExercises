3886ff5d2927eaad2f4c49dec1ce445ffa485777
pschake34 <pschake34@charlottesvilleschools.org>
2021-10-06 10:18:39 -0400

    Finish DigitDisplay

--- Loops/DigitDisplay/DigitDisplay.java
+++ Loops/DigitDisplay/DigitDisplay.java
@@ -3,11 +3,23 @@ import java.util.Scanner;
 public class DigitDisplay {
    public static void main(String[] args) {
       Scanner input = new Scanner(System.in);
-      int number;
+      long number;
+      long places = 1;
       
       System.out.print("Enter a positive integer: ");
-      number = input.nextInt();
+      number = input.nextLong();
+      long tempNumber = number;
       
+      while (tempNumber > 0) {
+         places *= 10;
+         tempNumber = number;
+         tempNumber /= places;
+      }
       
+      while (number > 0) {
+         places /= 10;
+         System.out.println(number / places);
+         number %= places;
+      }
    }
 }
