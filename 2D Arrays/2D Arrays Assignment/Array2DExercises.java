a7e35ef680db6ad8f655ada71e3839b013754cf0
pschake34 <pschake34@charlottesvilleschools.org>
2022-03-23 10:37:31 -0400

    Finish 2D Arrays assignment

--- 2D Arrays/2D Arrays Assignment/Array2DExercises.java
+++ 2D Arrays/2D Arrays Assignment/Array2DExercises.java
@@ -1,8 +1,8 @@
 public class Array2DExercises {
    public static int max(int[][] a) {
-      int max = 0;
+      int max = a[0][0];
       for (int i = 0; i < a.length; i++) {
-         for (int j = 0; j < a[i].length; i++) {
+         for (int j = 0; j < a[i].length; j++) {
             if (a[i][j] > max) {
                max = a[i][j];
             }
@@ -11,9 +11,123 @@ public class Array2DExercises {
    }
    
    public static int rowSum(int[][] a, int x) {
-      sum = 0;
+      int sum = 0;
       for (int i = 0; i < a[x].length; i++) {
          sum += a[x][i];
       } return sum;
+   }
+   
+   public static int columnSum(int[][] a, int x) {
+      int sum = 0;
+      for (int r = 0; r < a.length; r++) {
+         sum += a[r][x];
+      } return sum;
+   }
+   
+   public static int[] allRowSums(int[][] a) {
+      int[] rowSums = new int[a.length];
+      for (int r = 0; r < a.length; r++) {
+         rowSums[r] = rowSum(a, r);
+      } return rowSums;
+   }
+   
+   public static int[] allColumnSums(int[][] a) {
+      int[] columnSums = new int[a[0].length];
+      for (int c = 0; c < a.length; c++) {
+         columnSums[c] = columnSum(a, c);
+      } return columnSums;
+   }
+
+   
+   public static boolean isRowMagic(int[][] a) {
+      int[] rowSums = allRowSums(a);
+      int rowMagic = rowSums[0];
+      for (int i = 1; i < rowSums.length; i++) {
+         if (rowSums[i] != rowMagic) {
+            return false;
+         }
+      } return true;
+   }
+   
+   public static boolean isColumnMagic(int[][] a) {
+      int[] columnSums = allColumnSums(a);      
+      int colMagic = columnSums[0];
+      for (int i = 1; i < columnSums.length; i++) {
+         if (columnSums[i] != colMagic) {
+            return false;
+         }
+      } return true;
+   }
+   
+   public static boolean isSquare(int[][] a) {
+      int length = a.length;
+      for (int i = 0; i < length; i++) {
+         if (a[i].length != length) {
+            return false;
+         }
+      } return true;
+   }
+   
+   public static boolean isMagic(int[][] a) {
+      if (isSquare(a) && rowSum(a, 0) == columnSum(a, 0) && isRowMagic(a) && isColumnMagic(a)) {
+         int magicSum = rowSum(a, 0);
+         int diagSum = 0;
+         for (int i = 0; i < a.length; i++) {
+            diagSum += a[i][i];
+         } if (diagSum != magicSum) {
+            return false;
+         } diagSum = 0;
+         
+         for (int j = a.length-1; j >= 0; j--) {
+            diagSum += a[j][j];
+         } if (diagSum != magicSum) {
+            return false;
+         }
+      } else {
+         return false;
+      } return true;
+   }
+   
+   public static boolean isRowSequence(int[][] a, int x) {
+      int val = a[x][0];
+      for (int i = 0; i < a[x].length; i++) {
+         if (a[x][i] != val) {
+            return false;
+         } val++;
+      } return true;
+   }
+   
+   public static boolean isColumnSequence(int[][] a, int x) {
+      int val = a[0][x];
+      for (int i = 0; i < a.length; i++) {
+         if (a[i][x] != val) {
+            return false;
+         } val++;
+      } return true;
+   }
+
+   
+   public static boolean isLatin(int[][] a) {
+      if (isSquare(a)) {
+         for (int i = 0; i < a.length; i++) {
+            if (!isRowSequence(a, i) || !isColumnSequence(a, i)) {
+               return false;
+            }
+         }
+      } else {
+         return false;
+      } return true;
+   }
+   
+   public static boolean isSequence(int[][] a) {
+      if (isSquare(a) && a[0][0] == 1) {
+         for (int r = 0; r < a.length; r++) {
+            if (!isRowSequence(a, r) || a[r][a[r].length-1] % a.length != 0) {
+               return false;
+            }
+         }
+      } else {
+         return false;
+      } return true;
    }
 }
--- 2D Arrays/2D Arrays Assignment/Array2DExercisesTest.java
+++ 2D Arrays/2D Arrays Assignment/Array2DExercisesTest.java
@@ -3,12 +3,10 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
-import Array2DExercises.Array2DExercises;
-
 public class Array2DExercisesTest {
 
 	// example arrays for testing
-	private int[][] squarepos, squareneg, sorted, reverse, jagged, tall, wide, negative;
+	private int[][] latin, magic, rowMagic, colMagic, squarepos, squareneg, sorted, reverse, jagged, tall, wide, negative;
 		
 	@Before public void setUp() {
 	   sorted = new int[][] {
@@ -60,86 +58,86 @@ public class Array2DExercisesTest {
 		   {-5,-7,-13},
 		   {-20,-18,-1}
 	   };
+      rowMagic = new int[][] {
+         {1, 2, 3},
+         {4, 1, 1},
+         {2, 2, 2},
+         {6, 0, 0}
+      };
+      colMagic = new int[][] {
+         {1, 5, 3},
+         {4, 0, 2},
+         {3, 3, 3}
+      };
+      magic = new int[][] {
+         {1, 1, 1},
+         {1, 1, 1},
+         {1, 1, 1},
+      };
+      latin = new int[][] {
+         {1, 2, 3, 4},
+         {2, 3, 4, 5},
+         {3, 4, 5, 6},
+         {4, 5, 6, 7}
+      };
     }
     
    @Test public void testGetMax() {
       assertEquals(16, Array2DExercises.max(sorted));
+      assertEquals(-1, Array2DExercises.max(negative));
    }
    
    @Test public void testGetRowSum() {
       assertEquals(10, Array2DExercises.rowSum(sorted, 0));
    }
    
-   /*
-	@Test public void testGetTotal() {
-		assertEquals(136, Array2DExercises.getTotal(sorted));
+   @Test public void testGetColSum() {
+      assertEquals(28, Array2DExercises.columnSum(sorted, 0));
+   }
+   
+   @Test public void testGetRowSums() {
+      int[] testArray = {1, 5, 14, -16, 34, 40};
+      int[] toBeTested = Array2DExercises.allRowSums(tall);
+      for (int i = 0; i < toBeTested.length; i++) {
+         assertEquals(testArray[i], toBeTested[i]);
+      }
+   }
+   
+   @Test public void testIsRowMagic() {
+      assertEquals(false, Array2DExercises.isRowMagic(sorted));
+      assertEquals(true, Array2DExercises.isRowMagic(rowMagic));
+   }
+   
+   @Test public void testIsColMagic() {
+      assertEquals(false, Array2DExercises.isColumnMagic(sorted));
+      assertEquals(true, Array2DExercises.isColumnMagic(colMagic));
+   }
+   
+   @Test public void testIsSquareArray() {
+		assertEquals(true, Array2DExercises.isSquare(sorted));
+		assertEquals(false, Array2DExercises.isSquare(jagged));
+		assertEquals(false, Array2DExercises.isSquare(tall));
+		assertEquals(false, Array2DExercises.isSquare(wide));
 	}
 
-	@Test public void testGetAverage() {
-		assertEquals(8.5, Array2DExercises.getAverage(sorted), 0.00);
-	}
-	
-	@Test public void testGetRowTotal() {
-		assertEquals(10, Array2DExercises.getRowTotal(sorted, 0));
-	}
-	
-	@Test public void testGetColumnTotal() {
-		assertEquals(28, Array2DExercises.getColumnTotal(sorted, 0));
-	}
-	
-	@Test public void testGetHighestInRow() {
-		assertEquals(4, Array2DExercises.getHighestInRow(sorted, 0));
-	}
-	
-	@Test public void testGetLowestInRow() {
-		assertEquals(1, Array2DExercises.getLowestInRow(sorted, 0));
-	}
-	
-	@Test public void testGetHighestInColumn() {
-		assertEquals(13, Array2DExercises.getHighestInColumn(sorted, 0));
-	}
-	
-	@Test public void testGetLowestInColumn() {
-		assertEquals(1, Array2DExercises.getLowestInColumn(sorted, 0));
-	}
-	
-	@Test public void testGetBottomLeftToTopRightDiagonalSum() {
-		assertEquals(34, Array2DExercises.getBottomLeftToTopRightDiagonalSum(sorted));
-	}
-	
-	@Test public void testGetTopLeftToBottomRightDiagonalSum() {
-		assertEquals(34, Array2DExercises.getTopLeftToBottomRightDiagonalSum(sorted));
-	}
-	
-	@Test public void testIsArraySorted() {
-		assertEquals(true, Array2DExercises.isArraySorted(sorted));
-		assertEquals(false, Array2DExercises.isArraySorted(reverse));
-		assertEquals(false, Array2DExercises.isArraySorted(squarepos));
-		assertEquals(false, Array2DExercises.isArraySorted(squareneg));
-	}
-	
-	@Test public void testIsArrayReversed() {
-		assertEquals(true, Array2DExercises.isArrayReversed(reverse));
-	}
-	
-	@Test public void testIsJaggedArray() {
-		assertEquals(true, Array2DExercises.isJaggedArray(jagged));
-		assertEquals(false, Array2DExercises.isJaggedArray(wide));
-		assertEquals(false, Array2DExercises.isJaggedArray(squareneg));
-	}
-	
-	@Test public void testIsSquareArray() {
-		assertEquals(true, Array2DExercises.isSquareArray(sorted));
-		assertEquals(false, Array2DExercises.isSquareArray(jagged));
-		assertEquals(false, Array2DExercises.isSquareArray(tall));
-		assertEquals(false, Array2DExercises.isSquareArray(wide));
-	}
-	
-	@Test public void testIsRectangularArray() {
-		assertEquals(true, Array2DExercises.isRectangularArray(wide));
-		assertEquals(true, Array2DExercises.isRectangularArray(tall));
-		assertEquals(false, Array2DExercises.isRectangularArray(jagged));
-		assertEquals(true, Array2DExercises.isRectangularArray(squareneg));
-		assertEquals(true, Array2DExercises.isRectangularArray(squarepos));
-	} */
+   @Test public void testIsMagic() {
+      assertEquals(true, Array2DExercises.isMagic(magic));
+		assertEquals(false, Array2DExercises.isMagic(jagged));
+		assertEquals(false, Array2DExercises.isMagic(tall));
+		assertEquals(false, Array2DExercises.isMagic(wide));
+   }
+   
+   @Test public void testisLatin() {
+      assertEquals(true, Array2DExercises.isLatin(latin));
+		assertEquals(false, Array2DExercises.isMagic(jagged));
+		assertEquals(false, Array2DExercises.isMagic(sorted));
+		assertEquals(false, Array2DExercises.isMagic(wide));
+   }
+   
+   @Test public void isSequence() {
+      assertEquals(true, Array2DExercises.isSequence(sorted));
+		assertEquals(false, Array2DExercises.isSequence(jagged));
+		assertEquals(false, Array2DExercises.isSequence(tall));
+		assertEquals(false, Array2DExercises.isSequence(wide));
+   }
 }
